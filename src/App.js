2d9bd4f30301de016dbda560b5b8869f6ea7bf95
Leonardo Lugo <leolugo5@outlook.com>
2021-07-07 01:44:29 -0500

    First ready to launch

--- src/App.js
+++ src/App.js
@@ -4,43 +4,63 @@ import {useEffect, useState} from 'react'
 
 function App() {
 
-  //AQUI SE SOLICITA Y ORGANIZA LA GEOLOCALIZACION DEL USUARIO
+  const [latitude, setlatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [city, setCity] = useState(null);
+  const [country, setCountry] = useState("");
+  const [icon, setIcon] = useState(null);
+  const [txtDesc, setTxtDesc] = useState("");
+  const [tempC, setTemC] = useState(null);
+  const [tempF, setTempF] = useState(0);
+
+
+
+  const gotPosition = (position) => {
+    setlatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude)
+  };
+
+  const getLocation = async () => {
+    try {
+      navigator.geolocation.getCurrentPosition(gotPosition);
+      const reqInfo = await `http://api.weatherapi.com/v1/current.json?key=d602d1948ede425291845325210307&q=${latVal},${lonVal}`;
+      const response = await fetch(reqInfo);
+      const data = await response.json();
+      console.log(data)
+      //AQUI SE ASIGNAN LOS VALORES CORRESPONDIENTES
+      setCity(data.location.tz_id)
+      setCountry(data.location.country)
+      setIcon(data.current.condition.icon)
+      setTxtDesc(data.current.condition.text)
+      setTemC(data.current.temp_c)
+      setTempF(data.current.temp_f)
+    } catch (error) {
+      alert('sin acceso a la ubicacion')
+    }
+  };
+
+  const latVal = latitude;
+  const lonVal = longitude;
 
   useEffect(() => {
     getLocation();
-  })
-
-  const getLocation = () => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setPosDat({
-        longitude: position.coords.longitude,
-        latitude: position.coords.latitude
-      })},
-    function (error) {alert('No se pudo obtener su ubicación, Por favor recarge la pagina')})
-  };
-
-  const [posDat, setPosDat] = useState({
-    longitude: 0,
-    latitude: 0
-  })
-
-  // ESTA ES LA RESPUESTA DE LA APPI
-  
-  const infoRequest = async sendingUserInfo => {
-    const url = `http://api.weatherapi.com/v1/current.json?key=d602d1948ede425291845325210307&q=${posDat.latitude},${posDat.longitude}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data)
-  }
-  infoRequest()
+  });
+ 
   
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
-
+      <h1>The Weather App</h1>
+    
       <div className="diapay"> los datos son los siguientes </div>
-      <div>latitude: {posDat.latitude} </div>
-      <div>longitude: {posDat.longitude} </div>
+      <div>latitude: {latitude} </div>
+      <div>longitude: {longitude} </div>
+      <div>city: {city} </div>
+      <div>country: {country} </div>
+      <div> {icon} </div>
+      <div> description: {txtDesc} </div>
+      <div>Temperature °C: {tempC} </div>
+      <div>Temperature °F: {tempF} </div>
+      <button></button>
     </div>
   );
 }
