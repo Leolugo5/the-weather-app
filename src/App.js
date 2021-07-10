4e84eebdccd2640366eba455d3ce504b75a60f75
Leonardo Lugo <leolugo5@outlook.com>
2021-07-10 17:45:52 -0500

    final!!!! really finallllll

--- src/App.js
+++ src/App.js
@@ -14,42 +14,31 @@ function App() {
   const [tempF, setTempF] = useState(null);
 
 
-
-  const gotPosition = (position) => {
-    setlatitude(position.coords.latitude);
-    setLongitude(position.coords.longitude)
-  };
-
-  const getLocation = async () => {
-    try {
-      navigator.geolocation.getCurrentPosition(gotPosition);
-      const reqInfo = `http://api.weatherapi.com/v1/current.json?key=d602d1948ede425291845325210307&q=${latVal},${lonVal}`;
-      const response = await fetch(reqInfo);
+  useEffect(()=> {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setlatitude(position.coords.latitude)
+      setLongitude(position.coords.longitude)
+    })
+  }, [])
+  useEffect(()=> {
+    const request = async () => {
+      const link = `http://api.weatherapi.com/v1/current.json?key=d602d1948ede425291845325210307&q=${latitude},${longitude}&aqi=no`;
+      const response = await fetch(link);
       const data = await response.json();
-      
-      
-      //AQUI SE ASIGNAN LOS VALORES CORRESPONDIENTES
-      setCity(data.location.region)
+      setCity(data.location.name)
       setCountry(data.location.country)
       setIcon(data.current.condition.icon)
       setTxtDesc(data.current.condition.text)
       setTemC(data.current.temp_c)
       setTempF(data.current.temp_f)
-    } catch (error) {
-      alert('zcenlrrv')
+      console.log(data)
     }
-  };
-
-  const latVal = latitude;
-  const lonVal = longitude;
-
-  useEffect(() => {
-    getLocation();
-  });
-
-
+    if (latitude && longitude) {
+      request()
+    }
+  }, [latitude, longitude])
   
-
+  
 
   return (
     <div className="App">
